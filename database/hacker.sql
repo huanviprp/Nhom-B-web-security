803de3f2aae498c307ed79c3b47074f7f145b57e
AnPhan <tlt2542001@gmail.com>
2022-10-21 16:30:42 +0700

    fix loi csrf

---
+++ database/hacker.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th10 12, 2022 lúc 12:33 PM
+-- Phiên bản máy phục vụ: 5.7.36
+-- Phiên bản PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `hacker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hackdata`
+--
+
+DROP TABLE IF EXISTS `hackdata`;
+CREATE TABLE IF NOT EXISTS `hackdata` (
+  `sessionid` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `hackdata`
+--
+
+INSERT INTO `hackdata` (`sessionid`) VALUES
+('2'),
+('PHPSESSID=fq75c7ejm4buj98ctvd1f083mc'),
+('PHPSESSID=fq75c7ejm4buj98ctvd1f083mc');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ delete_user.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once 'models/UserModel.php';
+$userModel = new UserModel();
+
+$user = NULL; //Add new user
+$id = NULL;
+$token = md5($_SESSION['id']);
+
+if (!empty($_GET['id'])) {
+    if ($_GET['token'] == $token) {
+        $id = $_GET['id'];
+        $userModel->deleteUserById($id);
+    }
+}
+header('location: list_users.php');
